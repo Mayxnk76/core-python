0584c45fb5900709157e3173c297a8e972dd8a35
Mayank76x <mayankrahani@email.com>
2026-07-27 18:00:40 +0530

    Initial commit: add Python basics and README

---
+++ 1st.py
@@ -0,0 +1,13 @@
+
+# #Q1 Arithmethic Opretion#
+# num1 = int(input("enter first number: "))
+# num2 = int(input("enter second number: "))
+#
+# print("addition =",num1 + num2)
+# print("subtraction =",num1 - num2)
+# print("division =",num1 / num2)
+# print("multiplicatin =",num1 * num2)
+# print("moduls = ",num1 % num2)
+
+#Q2 Float number division #
+
