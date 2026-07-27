b3772cef0d1a207648142c9f22e5ad8bc658ec68
Mayank76x <mayankrahani@email.com>
2026-07-27 19:30:01 +0530

    Initial commit: add Python basics and README

--- 1st.py
+++ 1st.py
@@ -26,3 +26,15 @@ a, b = b, a
 print("After swaping")
 print("a =", a)
 print("b =", b)
+
+
+# Q4 Find net salary
+
+basic = int(input("Eneter basic salary:"))
+hra = int(input("Eneter hra :"))
+da =int(input("Eneter da :"))
+pf =int(input("Eneter pf :"))
+
+net = basic + hra + da - pf
+
+print("Net salary is :", net)
