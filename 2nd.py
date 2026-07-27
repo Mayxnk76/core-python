300c593256b16c21840b035cf484156175e6b3f9
Mayank76x <mayankrahani@email.com>
2026-07-27 19:42:03 +0530

    Initial commit: add Python code

---
+++ 2nd.py
@@ -0,0 +1,6 @@
+#Q2 Float number division
+
+n1 = float(input("enter first number: "))
+n2 = float(input("enter second number: "))
+
+print("integer division =",n1 // n2)
---
+++ 3rd.py
@@ -0,0 +1,10 @@
+# Q3 Swap two numbers
+
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+
+a, b = b, a
+
+print("After swaping")
+print("a =", a)
+print("b =", b)
