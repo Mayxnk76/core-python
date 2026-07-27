cfe0150d4c87e0172665a77f43adc7a8247e8d70
Mayank76x <mayankrahani@email.com>
2026-07-27 19:14:42 +0530

    Initial commit: add Python basics and README

--- 1st.py
+++ 1st.py
@@ -1,17 +1,29 @@
-#
-# #Q1 Arithmethic Opretion
-# num1 = int(input("enter first number: "))
-# num2 = int(input("enter second number: "))
-#
-# print("addition =",num1 + num2)
-# print("subtraction =",num1 - num2)
-# print("division =",num1 / num2)
-# print("multiplicatin =",num1 * num2)
-# print("moduls = ",num1 % num2)
+
+#Q1 Arithmethic Opretion
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+
+print("addition =",num1 + num2)
+print("subtraction =",num1 - num2)
+print("division =",num1 / num2)
+print("multiplicatin =",num1 * num2)
+print("moduls = ",num1 % num2)
 
 #Q2 Float number division
 
-num1 = float(input("enter first number: "))
-num2 = float(input("enter second number: "))
+n1 = float(input("enter first number: "))
+n2 = float(input("enter second number: "))
 
-print("integer division =",num1 // num2)
+print("integer division =",n1 // n
+2)
+
+# Q3 Swap two numbers
+
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+
+a, b = b, a
+
+print("After swaping")
+print("a =", a)
+print("b =", b)
