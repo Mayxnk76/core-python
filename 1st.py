19592d0d6e4c4e972b98ac26e5771e9ca1a030ee
Mayank76x <mayankrahani@email.com>
2026-07-28 16:08:12 +0530

    Initial commit: add Python code

--- 1st.py
+++ 1st.py
@@ -1,48 +1,54 @@
 
-#Q1 Arithmethic Opretion
-num1 = int(input("enter first number: "))
-num2 = int(input("enter second number: "))
+# #Q1 Arithmethic Opretion
+# num1 = int(input("enter first number: "))
+# num2 = int(input("enter second number: "))
+#
+# print("addition =",num1 + num2)
+# print("subtraction =",num1 - num2)
+# print("division =",num1 / num2)
+# print("multiplicatin =",num1 * num2)
+# print("moduls = ",num1 % num2)
+#
+# #Q2 Float number division
+#
+# n1 = float(input("enter first number: "))
+# n2 = float(input("enter second number: "))
+#
+# print("integer division =",n1 // n2)
+#
+# # Q3 Swap two numbers
+#
+# a = int(input("enter first number: "))
+# b = int(input("enter second number: "))
+#
+# a, b = b, a
+#
+# print("After swaping")
+# print("a =", a)
+# print("b =", b)
+#
+# # Q4 Find net salary
+#
+# basic = int(input("Eneter basic salary:"))
+# hra = int(input("Eneter hra :"))
+# da =int(input("Eneter da :"))
+# pf =int(input("Eneter pf :"))
+#
+# net = basic + hra + da - pf
+#
+# print("Net salary is :", net)
+#
+# # Q6 check even or odd
+#
+# num = int(input("enter number: "))
+#
+# if num % 2 == 0:
+#     print("even number")
+# else:
+#     print("odd number")
 
-print("addition =",num1 + num2)
-print("subtraction =",num1 - num2)
-print("division =",num1 / num2)
-print("multiplicatin =",num1 * num2)
-print("moduls = ",num1 % num2)
+ # Q7 print the square of 5 numbers
 
-#Q2 Float number division
-
-n1 = float(input("enter first number: "))
-n2 = float(input("enter second number: "))
-
-print("integer division =",n1 // n2)
-
-# Q3 Swap two numbers
-
-a = int(input("enter first number: "))
-b = int(input("enter second number: "))
-
-a, b = b, a
-
-print("After swaping")
-print("a =", a)
-print("b =", b)
-
-# Q4 Find net salary
-
-basic = int(input("Eneter basic salary:"))
-hra = int(input("Eneter hra :"))
-da =int(input("Eneter da :"))
-pf =int(input("Eneter pf :"))
-
-net = basic + hra + da - pf
-
-print("Net salary is :", net)
-
-# Q6 check even or odd
-
-num = int(input("enter number: "))
-
-if num % 2 == 0:
-    print("even number")
-else:
-    print("odd number")
+for i in range(5):
+        num = int(input("enter number: "))
+        print("Square =",num * num)
