ada6b4e7fb784075a5a1ad569dba7aea0c792fbd
Mayank76x <mayankrahani@email.com>
2026-07-28 19:57:04 +0530

    Initial commit: add Python code

--- loop practice.py
+++ loop practice.py
@@ -1,5 +1,18 @@
-# Q1 print the even number between 1 to 100 using loop
+# # Q1 print the even number between 1 to 100 using loop
+#
+# for i in range(2 , 101, 2):
+#     print(i)
 
-for i in range(2 , 101, 2):
-    print(i)
+# Q2 print the value is prime or not
 
+num = int(input("enter number: "))
+
+if num <= 1:
+    print("The number is not prime")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            print("The number is not prime")
+            break
+    else:
+        print("The number is prime")
