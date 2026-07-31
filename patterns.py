965e26776287160fa21dde6d16d7e700f63d761b
Mayank76x <mayankrahani@email.com>
2026-07-31 15:23:51 +0530

    Initial commit: add Python code

--- patterns.py
+++ patterns.py
@@ -1,27 +1,47 @@
-# # Q1 pattern
-# # *
-# # * *
-# # * * *
-# # * * * *
-# # * * * * *
-#
-#
-# for i in range(1, 6):
-#     print("* " * i)
+# Q1 pattern
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
 
-# # Q2 pattern
-# # *
-# # **
-# # ***
-# # ****
-# # *****
-# # ******
-# # *****
-# # ****
-# # ***
-# # **
-# # *
-# for i in range(1,6):
-#     print("*" * i)
-# for j in range(6, 0, -1):
-#     print("*" * j)
+
+for i in range(1, 6):
+    for j in range(i):
+        print(i, end=" ")
+    print()
+
+# Q2 pattern
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+# *****
+# ****
+# ***
+# **
+# *
+for i in range(1,6):
+    print("*" * i)
+for j in range(6, 0, -1):
+    print("*" * j)
+
+# Q3 pattern
+# *****
+# ****
+# ***
+# **
+# *
+
+for i in range(5, 0, -1):
+    print("*" * i)
+
+# Q4 pattern
+
+
+for i in range(1, 5):
+    for j in range(1, 5):
+        print("*", end=" ")
+    print()
