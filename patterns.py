8c423d2058d2c249a3c74d56b4b2876269e72cd2
Mayank76x <mayankrahani@email.com>
2026-08-01 19:37:14 +0530

    Initial commit: add Python code

--- patterns.py
+++ patterns.py
@@ -1,69 +1,69 @@
-# # Q1 pattern
-# # *
-# # * *
-# # * * *
-# # * * * *
-# # * * * * *
-#
-#
-# for i in range(6):
-#     for j in range(i):
-#         print("*", end=" ")
-#     print()
-#
-# # Q2 pattern
-# # *
-# # **
-# # ***
-# # ****
-# # *****
-# # ******
-# # *****
-# # ****
-# # ***
-# # **
-# # *
-# for i in range(1,6):
-#     print("*" * i)
-# for j in range(6, 0, -1):
-#     print("*" * j)
-#
-# # Q3 pattern
-# # *****
-# # ****
-# # ***
-# # **
-# # *
-#
-# for i in range(5, 0, -1):
-#     print("*" * i)
-#
-# for i in range(6):
-#     for j in range(i, 6):
-#         print("*", end=" ")
-#     print()
-#
-# # Q4 pattern
-#
-#
-# for i in range(1, 5):
-#     for j in range(1, 5):
-#         print("*", end=" ")
-#     print()
-#
-# # Q5 pattern
-#   #           *
-#   #         * *
-#   #       * * *
-#   #     * * * *
-#   #   * * * * *
-#   # * * * * * *
-# for i in range(6):
-#     for j in range(i, 6):
-#         print(" ", end=" ")
-#     for k in range(i + 1):
-#          print("*", end=" ")
-#     print()
+# Q1 pattern
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+
+for i in range(6):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+# Q2 pattern
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+# *****
+# ****
+# ***
+# **
+# *
+for i in range(1,6):
+    print("*" * i)
+for j in range(6, 0, -1):
+    print("*" * j)
+
+# Q3 pattern
+# *****
+# ****
+# ***
+# **
+# *
+
+for i in range(5, 0, -1):
+    print("*" * i)
+
+for i in range(6):
+    for j in range(i, 6):
+        print("*", end=" ")
+    print()
+
+# Q4 pattern
+
+
+for i in range(1, 5):
+    for j in range(1, 5):
+        print("*", end=" ")
+    print()
+
+# Q5 pattern
+  #           *
+  #         * *
+  #       * * *
+  #     * * * *
+  #   * * * * *
+  # * * * * * *
+for i in range(6):
+    for j in range(i, 6):
+        print(" ", end=" ")
+    for k in range(i + 1):
+         print("*", end=" ")
+    print()
 
 # Q7 pattern
 # * * * * * *
