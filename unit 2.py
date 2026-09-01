b106db88e0a4bf68ad246975d3fd4b01d1690e86
Mayank <mayankrahani@gmail.com>
2026-09-01 17:36:43 +0530

    unit 2 add

---
+++ unit 2.py
@@ -0,0 +1,16 @@
+# UNIT - 2
+# Program Control Flow and Strings basic programs
+
+# 1. Find the minimum and maximum of 2 numbers
+a = 10
+b = 20
+
+minimum = min(a,b)
+maximum = max(a,b)
+print(f"minimum :",minimum,"maximum :",maximum)
+# second trick
+if b > a:
+    print("the maximum number is :",b)
+    print("the minimum number is :",a)
+
+# 2. Write a program to find the maximum of three numbers
